96295b31b9414f56fd37e8d43741adb0bbd109b8
Vinicius Victor <vine.victordev@gmail.com>
2022-10-25 20:07:47 -0300

    Função Pegar Nome e Salario

---
+++ script.js
@@ -0,0 +1,11 @@
+// Lista de Exercicio - Aumento
+var nome, salario;
+
+PegaNomeSalario();
+
+
+function PegaNomeSalario() {
+    nome = prompt("Informe seu Nome:");
+    salario = parseInt(prompt("Informe o Salario:"));
+    return nome, salario;
+};
